4f0bc7da8caddd89b180e38980fc66a08e2df17c
Gabriel <gabriel.touzalin@gmail.com>
2023-05-01 12:10:06 +0200

    adde sphere scene

--- includes/config.h
+++ includes/config.h
@@ -6,14 +6,14 @@
 /*   By: gtouzali <gtouzali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 07:49:39 by gtouzali          #+#    #+#             */
-/*   Updated: 2023/05/01 11:11:40 by gtouzali         ###   ########.fr       */
+/*   Updated: 2023/05/01 12:05:14 by gtouzali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONFIG_H
 # define CONFIG_H
 
-# define WIDTH 200
+# define WIDTH 1000
 # define ASPECT_RATIO 9/16
 # define HEIGHT  WIDTH * ASPECT_RATIO
 
