3b4cdc4dae9f4d7fa9fdf5fb919f6a66d20bfd37
Gabriel <gabriel.touzalin@gmail.com>
2023-05-14 14:02:48 +0200

    ff branch

--- src/rotation.c
+++ src/rotation.c
@@ -6,7 +6,7 @@
 /*   By: gtouzali <gtouzali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 09:59:40 by gtouzali          #+#    #+#             */
-/*   Updated: 2023/05/14 13:59:56 by gtouzali         ###   ########.fr       */
+/*   Updated: 2023/05/14 14:01:39 by gtouzali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,7 @@ void rotation_x(t_scene *scene, double angle)
     t_obj_list	*cursor;
 
     cursor = scene->obj_list;
+    light_rot_x(scene->light, angle);
     while (cursor)
     {
         if (cursor->type == sphere)
@@ -119,6 +120,7 @@ void rotation_y(t_scene *scene, double angle)
     t_obj_list	*cursor;
 
     cursor = scene->obj_list;
+    light_rot_y(scene->light, angle);
     while (cursor)
     {
         if (cursor->type == sphere)
