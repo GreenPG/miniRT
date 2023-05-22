d6c6141cf54687fe70e619bffae40e96965aada5
Gillian Pasquette <gpasquet@student.42angouleme.fr>
2023-05-22 13:59:12 +0200

    ombre des sphere fini, probleme avec celle des plans. Dans la scene basic, le plan horizontal est trouve plus proche que la light lorsque l on monte

--- src/calculations/light_calculations.c
+++ src/calculations/light_calculations.c
@@ -6,13 +6,13 @@
 /*   By: gpasquet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 16:48:50 by gpasquet          #+#    #+#             */
-/*   Updated: 2023/05/19 10:45:27 by gpasquet         ###   ########.fr       */
+/*   Updated: 2023/05/22 13:57:44 by gpasquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 
-static double	sphere_shadow(t_sphere *sphere, t_light *light, t_vector light_dir)
+static double	sphere_shadow(t_sphere *sphere, t_normal normal, t_vector light_dir)
 {
 	t_vector	oc;
 	double		a;
@@ -20,9 +20,9 @@ static double	sphere_shadow(t_sphere *sphere, t_light *light, t_vector light_dir
 	double		c;
 	double		d;
 
-	oc.x = light->origin->x - sphere->origin->x;
-	oc.y = light->origin->y - sphere->origin->y;
-	oc.z = light->origin->z - sphere->origin->z;
+	oc.x = normal.origin.x - sphere->origin->x;
+	oc.y = normal.origin.y - sphere->origin->y;
+	oc.z = normal.origin.z - sphere->origin->z;
 	a = dot_product(light_dir, light_dir);
 	b = dot_product(oc, light_dir);
 	c = dot_product(oc, oc) - ((sphere->diameter / 2) * (sphere->diameter / 2));
@@ -48,31 +48,37 @@ static double	plane_shadow(t_plane *plane, t_vector light_dir)
 	return (INFINITY);
 }
 
-static int	light_intersect(t_scene *scene, t_vector light_dir)
+static double	get_light_distance(t_light *light, t_normal normal)
+{
+	double		light_distance;
+	t_vector	hit_to_light;
+
+	hit_to_light.x = light->origin->x - normal.origin.x;
+	hit_to_light.y = light->origin->y - normal.origin.y;
+	hit_to_light.z = light->origin->z - normal.origin.z;
+	light_distance = sqrt(dot_product(hit_to_light, hit_to_light));
+	return (light_distance);
+}
+
+static int	light_intersect(t_scene *scene, t_vector light_dir, t_normal normal)
 {
 	t_obj_list	*cursor;
 	double		distance;
-	double		obj_distance;
-	double		nearest_distance;
+	double		light_source_d;
 
-	light_dir.x *= -1;
-	light_dir.y *= -1;
-	light_dir.z *= -1;
+	light_source_d = get_light_distance(scene->light, normal);
 	cursor = scene->obj_list;
-	distance = INFINITY;
-	nearest_distance = INFINITY;
-	obj_distance = -INFINITY;
+	distance = 0;
 	while (cursor)
 	{
-		if (cursor->type == sphere)
-			distance = sphere_shadow(cursor->sphere, scene->light, light_dir);
-		else if (cursor->type == plane)
-			distance = plane_shadow(cursor->plane, light_dir);
-		if (cursor->hitted == 1)
-			obj_distance = distance;
-		else
-			nearest_distance = distance;
-		if (nearest_distance < obj_distance)
+		if (cursor->hitted == 0)
+		{
+			if (cursor->type == sphere)
+				distance = sphere_shadow(cursor->sphere, normal, light_dir);
+			if (cursor->type == plane)
+				distance = plane_shadow(cursor->plane, light_dir);
+		}
+		if (distance > 0 && distance < light_source_d)
 			return (1);
 		cursor = cursor->next;
 	}
@@ -103,7 +109,7 @@ int	get_diffuse_ratio(t_scene *scene, t_normal normal)
 	int			b;
 
 	light_dir = get_light_dir(scene->light, normal);
-	if (light_intersect(scene, light_dir) == 1)
+	if (light_intersect(scene, light_dir, normal) == 1)
 		return (0);
 	diffuse_ratio = dot_product(normal.dir, light_dir);
 	diffuse_ratio = fmax(0.0, diffuse_ratio);
